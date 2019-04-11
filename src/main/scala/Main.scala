cd1ebedfa459b122b752a8c6c9f87ac8349f3de5
MikeKoms <ponilol37@gmail.com>
2019-04-11 11:23:56 +0300

    OTTS WE LOVE YOU! (and especially your Tethys)

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,18 +1,31 @@
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.apache.kafka.common.serialization.StringDeserializer
 import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.types.{DataTypes, StructType, TimestampType}
 import org.apache.spark.streaming.dstream.{DStream, InputDStream}
 import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
 import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
 import org.apache.spark.streaming.kafka010._
 import org.apache.spark.streaming.{Seconds, StreamingContext}
+import tethys.{JsonReader, _}
+import tethys.derivation.semiauto._
+import tethys.jackson._
 
 
 object Main {
+
   case class Message(message: String, status: String, eventType: String, timeStamp: TimestampType)
 
+
   def main(args: Array[String]): Unit = {
+    implicit val barReader: JsonReader[Message] = jsonReader[Message]
+
+    val spark = SparkSession
+      .builder()
+      .appName("KafkaConsumer")
+      .getOrCreate()
+
 
     val kafkaParams = Map[String, Object](
       "bootstrap.servers" -> "t510:9092",
@@ -41,19 +54,16 @@ object Main {
       .add("timestamp", DataTypes.TimestampType)
 
     // где - то тут парсится json
-    stream.foreachRDD { (rdd, time) =>
-      //val data = rdd.map(record => record.value)
-      //val json = spark.read//.schema(struct)//.json(data)
-      //val result = json.groupBy($"action").agg(count("*").alias("count"))
-      //result.show
+    stream.foreachRDD { rdd =>
+      val data = rdd.map(_.value()).map(json => json.jsonAs[Message])
     }
 
     // это часть осталась от простейшего консумера, который просто на экран выводит все, что приходит
     // как научимся json парсить, выпилим этот показательный примерчик
-    val results: DStream[(String, String)] = stream.map(record => (record.key, record.value))
+    /*val results: DStream[(String, String)] = stream.map(record => (record.key, record.value))
     val lines: DStream[String] = results.map(tuple => tuple._2)
     val words = lines.flatMap(x => x.split(""))
-    lines.print()
+    lines.print()*/
 
     ssc.start()
 
